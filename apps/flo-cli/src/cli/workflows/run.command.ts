1765bad090c82a915c491f1596829b515846caf6
Floyd Haremsa <floyd.h@gmx.de>
2023-10-15 03:40:02 +0200

    fix: fix config service init issue

--- apps/flo-cli/src/cli/workflows/run.command.ts
+++ apps/flo-cli/src/cli/workflows/run.command.ts
@@ -8,29 +8,35 @@ import { runWorkflow } from '../../lib/workflows/run-workflow'
 export const runCommand = new Command()
     .createCommand('run')
     .description('Run predefined workflows, see Commands for avalable workflows')
-    .action(() => runCommand.help())
+    .action(() => {
+        const configService = ConfigService.getInstance()
 
-const workflows = ConfigService.getInstance().config.workflows || []
-workflows.forEach(workflow =>
-    runCommand
-        .command(workflow.workflowId)
-        .aliases(workflow.aliases || [])
-        .description([workflow.name, workflow.description].filter(Boolean).join(' - '))
-        .option('-y, --yes', 'Run all steps without confirmation (takes precedence over `--confirm`)')
-        .option('-c, --confirm', 'Force confirmation dialog')
-        .option('-d, --dry', 'Dry run, list all steps without running them')
-        .action((opts: { yes?: boolean; confirm?: boolean; dry?: boolean }) => {
-            const resolvedWorkflow = resolveWorkflow(workflow)
+        const subCommand = new Command('run')
+        const workflows = configService.config.workflows || []
+        workflows.forEach(workflow =>
+            subCommand
+                .command(workflow.workflowId)
+                .aliases(workflow.aliases || [])
+                .description([workflow.name, workflow.description].filter(Boolean).join(' - '))
+                .option('-y, --yes', 'Run all steps without confirmation (takes precedence over `--confirm`)')
+                .option('-c, --confirm', 'Force confirmation dialog')
+                .option('-d, --dry', 'Dry run, list all steps without running them')
+                .action(async (opts: { yes?: boolean; confirm?: boolean; dry?: boolean }) => {
+                    const resolvedWorkflow = resolveWorkflow(workflow)
 
-            if (opts.dry) {
-                Logger.log().log('Steps for ' + resolvedWorkflow.name)
-                printStepsOf(resolvedWorkflow)
-                Logger.log()
-                return
-            }
+                    if (opts.dry) {
+                        Logger.log().log('Steps for ' + resolvedWorkflow.name)
+                        printStepsOf(resolvedWorkflow)
+                        Logger.log()
+                        return
+                    }
 
-            Logger.log()
-            runWorkflow(resolvedWorkflow, opts)
-            Logger.log()
-        }),
-)
+                    Logger.log()
+                    await runWorkflow(resolvedWorkflow, opts)
+                    Logger.log()
+                }),
+        )
+
+        const arg = process.argv.filter(arg => arg !== 'run' && arg !== '--debug')
+        subCommand.parse(arg)
+    })
